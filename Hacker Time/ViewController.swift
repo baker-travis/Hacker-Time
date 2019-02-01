4ef511ce8d6026d9fb18fcb33cc0d668eb3bd885
Travis Baker <baker.travis.w@gmail.com>
2019-01-31 20:40:04 -0600

    Initial Commit

---
+++ Hacker Time/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hacker Time
+//
+//  Created by Travis Baker on 1/31/19.
+//  Copyright © 2019 Travis Baker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
